5b66709ea4eeddb62b55d5553c0244c67aa827e4
Ycreak <y.creak@hotmail.com>
2017-05-24 16:29:08 +0200

    Integratie MySQL database

---
+++ app/src/main/java/com/example/philippebors/volgjevrienden/Config.java
@@ -0,0 +1,25 @@
+package com.example.philippebors.volgjevrienden;
+
+/**
+ * Created by luukie on 24/05/2017.
+ */
+
+//package net.simplifiedcoding.spinnerexample;
+
+/**
+ * Created by Belal on 10/31/2015.
+ */
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "http://nolden.biz/Android/json.php";
+
+    //Tags used in the JSON String
+    public static final String TAG_USERNAME = "ID";
+    public static final String TAG_NAME = "NAME";
+    public static final String TAG_COURSE = "PHONENUMBER";
+    public static final String TAG_SESSION = "LONGITUDE";
+    public static final String TAG_LATITUDE = "LATITUDE";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+}
